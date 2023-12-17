23c645f9bfb0bb12674c6abff157386b986010e5
Vaishak234 <102749929+Vaishak234@users.noreply.github.com>
2023-12-17 14:31:15 +0530

    Update baseAxios.js

--- client/src/api/baseAxios.js
+++ client/src/api/baseAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export default axios.create({
-    baseURL: 'https://users-crud-backend-xaaj.onrender.com/api',
+    baseURL:'https://users-crud-backend-xaaj.onrender.com/api',
     headers: { 'Content-Type': 'application/json' },
     withCredentials:true
 })
